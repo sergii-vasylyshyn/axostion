36cbae6a92ed6e3442b8b5294c985c1bf3e3e891
Sergii Vasylyshyn <sergii.vasylyshyn@p-product.com>
2021-10-10 07:58:19 +0300

    refactor gdrive

--- js/lib/gdrive.js
+++ js/lib/gdrive.js
@@ -1,56 +1,60 @@
-function initClient() {
+const initClient = () => {
     gapi.client.init({
-        // Ваш ключ API
         apiKey: "AIzaSyA4AgMpx_tppq5Jx0MJLqyOXEyG86fo1wA",
-
-        // Ваш идентификатор клиента
         clientId: "1016671593130-0rq9r788h513nm0ga9i3cn1d090n9v6c.apps.googleusercontent.com",
-
-        // Указание, что мы хотим использовать Google Drive API v3
         discoveryDocs: ['https://www.googleapis.com/discovery/v1/apis/drive/v3/rest'],
-
-        // Запрос доступа к application data folder (см. ниже)
         scope: 'https://www.googleapis.com/auth/drive.appfolder'
 
     }).then(() => {
-        // Начинаем ловить события логина/логаута (см. ниже)
         gapi.auth2.getAuthInstance().isSignedIn.listen(onSignIn)
-        // инициализация приложения
         initApp()
 
     }, error => {
         console.log('Failed to init GAPI client', error)
-        // работаем без гугла
         initApp({showAlert: 'google-init-failed-alert'})
     })
 }
 
-function isGapiLoaded() {
+const isGapiLoaded = () => {
     return gapi && gapi.auth2
 }
 
-function logIn() {
+const logIn = () => {
     if (isGapiLoaded()) {
-        // откроется стандартное окно Google с выбором аккаунта
         gapi.auth2.getAuthInstance().signIn()
     }
 }
 
-function logOut() {
+const logOut = () => {
     if (isGapiLoaded()) {
         gapi.auth2.getAuthInstance().signOut()
     }
 }
 
-function isLoggedIn() {
+const isLoggedIn = () => {
     return isGapiLoaded() && gapi.auth2.getAuthInstance().isSignedIn.get()
 }
 
-function onSignIn() {
+const onSignIn = () => {
     if (isLoggedIn()) {
-        // пользователь зашел
+
     } else {
-        // пользователь вышел
+        
     }
-    // пример реализации см. ниже в разделе "Синхронизация"
+}
+
+const prom = (gapiCall, argObj) => {
+    return new Promise((resolve, reject) => {
+        gapiCall(argObj).then(resp => {
+            if (resp && (resp.status < 200 || resp.status > 299)) {
+                console.log('GAPI call returned bad status', resp)
+                reject(resp)
+            } else {
+                resolve(resp)
+            }
+        }, err => {
+            console.log('GAPI call failed', err)
+            reject(err)
+        })
+    })
 }
